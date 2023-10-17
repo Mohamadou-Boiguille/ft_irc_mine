5e311e21911cd0bbf8a092c7e2e28d33335a325f
Mohamadou-Boiguille <m.boiguille@live.fr>
2023-10-17 18:12:06 +0200

    test

---
+++ Makefile
@@ -0,0 +1,33 @@
+CC = g++
+CFLAGS = -g3 -std=c++98 -Wall -Wextra -Werror
+SRCS_DIR = srcs
+OBJS_DIR = .obj
+NAME = ft_irc
+
+SRCS_LIST = $(wildcard $(SRCS_DIR)/**/*.cpp) $(wildcard $(SRCS_DIR)/*.cpp)
+OBJS_LIST = $(patsubst $(SRCS_DIR)/%.cpp,$(OBJS_DIR)/%.o,$(SRCS_LIST))
+
+DEP_LIST = $(patsubst $(SRCS_DIR)/%.cpp,$(OBJS_DIR)/%.d,$(SRCS_LIST))
+
+.PHONY: all clean fclean re
+
+all: $(NAME)
+
+$(NAME): $(OBJS_LIST)
+	$(CC) $(CFLAGS) $^ -o $@
+
+$(OBJS_DIR)/%.o: $(SRCS_DIR)/%.cpp
+	mkdir -p $(dir $@)
+	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
+
+clean:
+	rm -rf $(OBJS_DIR)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+-include $(DEP_LIST)
+
+
---
+++ srcs/commands/User.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   User.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zrebhi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 23:24:53 by zrebhi            #+#    #+#             */
+/*   Updated: 2023/10/14 00:44:05 by zrebhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Command.hpp"
+#include <cctype>
+#include <iostream>
+#include <map>
+
+void Command::user() {
+	std::string &username = _commandArray[1];
+	size_t firstSpace = username.find(' ');
+
+	std::cout << "username: " << username << std::endl;
+	if (username.empty())
+		username = "guest";
+	else if (firstSpace != username.npos)
+		username = username.substr(0, firstSpace);
+	std::cout << "username: " << username << std::endl;
+	this->_client.setUsername(username);
+}
+
+void Command::nick() {
+	std::string newNickname = this->_commandArray[1];
+	std::string oldNickname = this->_client.getNickname();
+
+	if (oldNickname.empty())
+		// this->_client.setNickname(newNickname);
+		oldNickname = '*';
+	if (nicknameAvailable(newNickname) && nicknameIsValid(newNickname)) {
+		this->_client.setNickname(newNickname);
+		this->_client.setRegistered(NICK_REGISTRATION);
+		// ft_send(this->_client, ":" + oldNickname + " NICK :" + newNickname);
+	}
+}
+
+bool Command::nicknameAvailable(std::string nickname) {
+	std::map<int, Client> clientList = this->_ircServ.getClientList();
+	std::map<int, Client>::iterator it = clientList.begin();
+
+	for (; it != clientList.end(); ++it) {
+		if (it->second.getNickname() == nickname && it->first != this->_client.getSocket()) {
+			ft_send(this->_client, ERR_NICKNAMEINUSE(this->_client));
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Command::nicknameIsValid(std::string nickname) {
+	std::string nonAlnumValidChars = "-_^[]{}\\`|";
+	if (nickname.at(0) == '-')
+		return false;
+	for (size_t i = 0; i < nickname.length(); i++)
+	{
+		char letter = nickname.at(i);
+		if (!isalnum(letter) && nonAlnumValidChars.find(letter) == std::string::npos)
+		{
+			ft_send(this->_client, ERR_ERRONEUSNICKNAME(nickname));
+			return false;
+		}
+	}
+	return true;
+}
